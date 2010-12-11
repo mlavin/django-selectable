e45b6a91e3c6c72a33459160957991185b3c11cc
mlavin <markdlavin@gmail.com>
2010-12-11 18:06:51 -0500

    Add support for allow_new parameter.

--- selectable/forms/widgets.py
+++ selectable/forms/widgets.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 
 __all__ = (
@@ -10,8 +11,10 @@ class AutoCompleteWidget(forms.TextInput):
 
     def __init__(self, lookup_class, *args, **kwargs):
         self.lookup_class = lookup_class
+        self.allow_new = kwargs.pop('allow_new', False)
         super(AutoCompleteWidget, self).__init__(*args, **kwargs)
         # New attrs
         url = lookup_class.url()
-        self.attrs['data-selectable-url'] = url
+        self.attrs[u'data-selectable-url'] = url
+        self.attrs[u'data-selectable-allow-new'] = str(self.allow_new).lower()
 
--- selectable/static/js/jquery.dj.selectable.js
+++ selectable/static/js/jquery.dj.selectable.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     $(":input[data-selectable-url]").each(function(i, elem) {
         var url = $(elem).attr('data-selectable-url');
-        $(elem).autocomplete({source: url});
+        $(elem).autocomplete({
+            source: url,
+            change: function(event, ui) {
+                var allowAttr = $(this).attr('data-selectable-allow-new');
+                var allowNew = false;
+                if (typeof allowAttr !== 'undefined' && allowAttr === 'true') {
+                    allowNew = true;
+                }
+                if (!ui.item) {
+                    if (!allowNew) {
+                        $(this).val("");
+					    $(this).data("autocomplete").term = "";
+					    return false;
+                    }
+                }
+            }
+        });
     });
 });
