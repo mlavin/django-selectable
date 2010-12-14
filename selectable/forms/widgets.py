603a331268964040e5e65fc3e76ee9d575a0a4d5
mlavin <markdlavin@gmail.com>
2010-12-13 21:38:42 -0500

    Later binding of new widget attributes.

--- selectable/forms/widgets.py
+++ selectable/forms/widgets.py
@@ -16,12 +16,13 @@ class AutoCompleteWidget(forms.TextInput):
         self.lookup_class = lookup_class
         self.allow_new = kwargs.pop('allow_new', False)
         super(AutoCompleteWidget, self).__init__(*args, **kwargs)
-        # New attrs
-        url = lookup_class.url()
-        self.attrs[u'data-selectable-url'] = url
-        self.attrs[u'data-selectable-type'] = 'text'
-        self.attrs[u'data-selectable-allow-new'] = str(self.allow_new).lower()
 
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        attrs = super(AutoCompleteWidget, self).build_attrs(extra_attrs, **kwargs)
+        attrs[u'data-selectable-url'] = self.lookup_class.url()
+        attrs[u'data-selectable-type'] = 'text'
+        attrs[u'data-selectable-allow-new'] = str(self.allow_new).lower()
+        return attrs
 
 class AutoCompleteSelectWidget(forms.MultiWidget):
 
@@ -48,6 +49,11 @@ class AutoComboboxWidget(AutoCompleteWidget):
     def __init__(self, lookup_class, *args, **kwargs):
         super(AutoComboboxWidget, self).__init__(lookup_class, *args, **kwargs)
         self.attrs[u'data-selectable-type'] = 'combobox'
+
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        attrs = super(AutoComboboxWidget, self).build_attrs(extra_attrs, **kwargs)
+        attrs[u'data-selectable-type'] = 'combobox'
+        return attrs
 
 
 class AutoComboboxSelectWidget(forms.MultiWidget):
