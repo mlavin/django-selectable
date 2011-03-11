41499002129e4c881be7d623c35f66bc20450e5f
mlavin <markdlavin@gmail.com>
2011-03-11 12:10:32 -0500

    Started sample project skeleton.

---
+++ example/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^example/', include('example.core.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
