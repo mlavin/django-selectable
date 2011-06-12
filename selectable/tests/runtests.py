b05bbdf8854b6dbe3b0de02cef25716d97827bad
mlavin <markdlavin@gmail.com>
2011-06-12 16:14:43 -0400

    Add app test runner.

---
+++ selectable/tests/runtests.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+from django.conf import settings
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test.db',
+            }
+        },
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'selectable',
+        ),
+        ROOT_URLCONF='selectable.tests.urls',
+    )
+
+
+from django.test.utils import get_runner
+
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['selectable']
+    parent = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", )
+    sys.path.insert(0, parent)
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
+
