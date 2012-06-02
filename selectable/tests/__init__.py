d7ffa1e59832b8fe81b51bbf0c5eb908e20d9418
mlavin <markdlavin@gmail.com>
2012-06-02 15:33:03 -0400

    Test cases for new template tags.

--- selectable/tests/__init__.py
+++ selectable/tests/__init__.py
@@ -41,5 +41,6 @@ from selectable.tests.decorators import *
 from selectable.tests.fields import *
 from selectable.tests.functests import *
 from selectable.tests.forms import *
+from selectable.tests.templatetags import *
 from selectable.tests.views import *
 from selectable.tests.widgets import *
