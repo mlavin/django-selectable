5a66e38dba4a9d81d8a905efa3336a0eb8b6776a
mlavin <markdlavin@gmail.com>
2012-09-07 12:15:17 -0400

    Less verbose package description.

--- selectable/__init__.py
+++ selectable/__init__.py
@@ -1,5 +1,4 @@
-"""
-django-selectable is a collection of tools and widgets for using/creating auto-complete selection widgets using Django and jQuery UI.
-"""
+"Auto-complete selection widgets using Django and jQuery UI."
+
 
 __version__ = '0.6.0dev'
