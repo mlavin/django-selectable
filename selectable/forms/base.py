a9059f075bc1bb48422a3aba564a38071b1acf9f
mlavin <markdlavin@gmail.com>
2012-09-20 20:22:09 -0400

    Move page cleaning logic to the form.
    --HG--
    branch : result-refactor

--- selectable/forms/base.py
+++ selectable/forms/base.py
@@ -8,6 +8,7 @@ __all__ = ('BaseLookupForm', )
 class BaseLookupForm(forms.Form):
     term = forms.CharField(required=False)
     limit = forms.IntegerField(required=False, min_value=1)
+    page = forms.IntegerField(required=False, min_value=1)
 
     def clean_limit(self):
         "Ensure given limit is less than default if defined"
@@ -16,3 +17,7 @@ class BaseLookupForm(forms.Form):
             (not limit or limit > settings.SELECTABLE_MAX_LIMIT)):
             limit = settings.SELECTABLE_MAX_LIMIT
         return limit
+
+    def clean_page(self):
+        "Return the first page if no page or invalid number is given."
+        return self.cleaned_data.get('page', 1) or 1
