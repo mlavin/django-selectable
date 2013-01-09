be06d98df9a3666090479510ab18bb919c1dbe3f
mlavin <markdlavin@gmail.com>
2013-01-09 18:51:29 -0500

    Fix url imports to remove 1.5 deprecation warnings.

--- selectable/urls.py
+++ selectable/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import handler404, handler500, patterns, url
+except ImportError:
+    # Django < 1.4
+    from django.conf.urls.defaults import handler404, handler500, patterns, url
 
 from selectable import registry
 
