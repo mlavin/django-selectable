1d99731c22df3d0cb16237e915054b4e2aace5cd
mlavin <markdlavin@gmail.com>
2013-01-31 23:17:22 -0500

    Add grunt configuration for running QUnit tests. Include documentation notes.

---
+++ grunt.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    qunit: {
+      urls: [
+        'http://localhost:<%= server.port %>/selectable/tests/qunit/index.html?jquery=1.9.0&ui=1.10.0',
+        'http://localhost:<%= server.port %>/selectable/tests/qunit/index.html?jquery=1.8.3&ui=1.9.2',
+        'http://localhost:<%= server.port %>/selectable/tests/qunit/index.html?jquery=1.7.2&ui=1.8.24',
+        'http://localhost:<%= server.port %>/selectable/tests/qunit/index.html?jquery=1.6.4&ui=1.8.24'
+      ]
+    },
+    lint: {
+      files: ['selectable/static/selectable/js/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        undef: true,
+        trailing: true,
+        indent: 4
+      },
+      globals: {
+        jQuery: true,
+        // Django admin globals
+        django: true,
+        dismissAddAnotherPopup: true,
+        windowname_to_id: true,
+        html_unescape: true,
+        // Optional globals
+        djselectableAdminPatch: true,
+        djselectableAutoLoad: true
+      }
+    },
+    server: {
+      port: 8085
+    },
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'server lint qunit');
+
+};
