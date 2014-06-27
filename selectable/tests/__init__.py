a88eb2c7fc2c2d875836f0a4c201ede0c082aceb
mlavin <markdlavin@gmail.com>
2014-06-26 20:46:09 -0400

    Update the test model definitions.

--- selectable/tests/__init__.py
+++ selectable/tests/__init__.py
@@ -1,37 +1,32 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 from ..base import ModelLookup
 from ..registry import registry
 
 
+@python_2_unicode_compatible
 class Thing(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
 
-    def __unicode__(self):
-        return self.name
-
     def __str__(self):
         return self.name
 
 
+@python_2_unicode_compatible
 class OtherThing(models.Model):
     name = models.CharField(max_length=100)
     thing = models.ForeignKey(Thing)
 
-    def __unicode__(self):
-        return self.name
-
     def __str__(self):
         return self.name
 
 
+@python_2_unicode_compatible
 class ManyThing(models.Model):
     name = models.CharField(max_length=100)
     things = models.ManyToManyField(Thing)
-
-    def __unicode__(self):
-        return self.name
 
     def __str__(self):
         return self.name
