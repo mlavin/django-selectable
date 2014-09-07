49155b4c357198deedc0bd95902534cd108d5ca7
mlavin <markdlavin@gmail.com>
2014-09-07 11:27:19 -0400

    Add wheel support and update PyPi classifiers.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
         'Operating System :: OS Independent',
