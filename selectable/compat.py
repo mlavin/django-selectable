368032d9bf3b9d65e433a058de43fd7b58d684c1
mlavin <markdlavin@gmail.com>
2015-01-25 13:42:13 -0500

    Removing more Django/Python compatibility code.

--- selectable/compat.py
+++ selectable/compat.py
@@ -1,19 +1,6 @@
 "Compatibility utilites for Python/Django versions."
-import sys
 
 try:
     from urllib.parse import urlparse
 except ImportError:
     from urlparse import urlparse
-
-PY3 = sys.version_info[0] == 3
-
-if PY3:
-    string_types = str,
-else:
-    string_types = basestring,
-
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
