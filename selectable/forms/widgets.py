c25a6b7abac078e3d9abdd8474fb6a1a26007626
Luke Plant <L.Plant.98@cantab.net>
2017-05-31 11:43:34 +0300

    Fixed broken 'title' attribute for LookupMultipleHiddenInput on Django 1.11

--- selectable/forms/widgets.py
+++ selectable/forms/widgets.py
@@ -206,36 +206,68 @@ class LookupMultipleHiddenInput(CompatMixin, forms.MultipleHiddenInput):
         self.lookup_class = import_lookup_class(lookup_class)
         super(LookupMultipleHiddenInput, self).__init__(*args, **kwargs)
 
+    # This supports Django 1.11 and later
+    def get_context(self, name, value, attrs):
+        lookup = self.lookup_class()
+        values = self._normalize_value(value)
+        values = list(values)  # force evaluation
+
+        context = super(LookupMultipleHiddenInput, self).get_context(name, values, attrs)
+
+        # Now override/add to what super() did:
+        subwidgets = context['widget']['subwidgets']
+        for widget_ctx, item in zip(subwidgets, values):
+            input_value, title = self._lookup_value_and_title(lookup, item)
+            widget_ctx['value'] = input_value  # override what super() did
+            if title:
+                widget_ctx['attrs']['title'] = title
+        return context
+
+    # This supports Django 1.10 and earlier
     def render(self, name, value, attrs=None, choices=()):
         lookup = self.lookup_class()
-        if value is None:
-            value = []
+        value = self._normalize_value(value)
 
         base_attrs = dict(self.attrs, type=self.input_type, name=name)
-        final_attrs = self.build_attrs_compat(base_attrs, attrs)
-        id_ = final_attrs.get('id', None)
+        combined_attrs = self.build_attrs_compat(base_attrs, attrs)
+        id_ = combined_attrs.get('id', None)
         inputs = []
-        model = getattr(self.lookup_class, 'model', None)
         for i, v in enumerate(value):
-            item = None
-            if model and isinstance(v, model):
-                item = v
-                v = lookup.get_item_id(item)
-            input_attrs = dict(value=force_text(v), **final_attrs)
+            input_attrs = combined_attrs.copy()
+            v_, title = self._lookup_value_and_title(lookup, v)
+            input_attrs.update(
+                value=v_,
+                title=title,
+            )
             if id_:
                 # An ID attribute was given. Add a numeric index as a suffix
                 # so that the inputs don't all have the same ID attribute.
                 input_attrs['id'] = '%s_%s' % (id_, i)
-            if v:
-                item = item or lookup.get_item(v)
-                input_attrs['title'] = lookup.get_item_value(item)
             inputs.append('<input%s />' % flatatt(input_attrs))
         return mark_safe('\n'.join(inputs))
 
+    # These are used by both paths
     def build_attrs_extra(self, attrs):
         attrs = super(LookupMultipleHiddenInput, self).build_attrs_extra(attrs)
         attrs['data-selectable-type'] = 'hidden-multiple'
         return attrs
+
+    def _normalize_value(self, value):
+        if value is None:
+            value = []
+        return value
+
+    def _lookup_value_and_title(self, lookup, v):
+        model = getattr(self.lookup_class, 'model', None)
+        item = None
+        if model and isinstance(v, model):
+            item = v
+            v = lookup.get_item_id(item)
+        title = None
+        if v:
+            item = item or lookup.get_item(v)
+            title = lookup.get_item_value(item)
+        return force_text(v), title
 
 
 class _BaseMultipleSelectWidget(SelectableMultiWidget, SelectableMediaMixin):
