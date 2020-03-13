125d2f5e3f17b3ee3b4813bebff8afe5968b6153
Kevi Nasto <kevin.nasto@nerdery.com>
2020-03-13 11:24:20 -0500

    If value is filtered out in the queryset, try the other values before returning None which fixes validation error

--- selectable/base.py
+++ selectable/base.py
@@ -153,6 +153,10 @@ class ModelLookup(LookupBase):
             try:
                 item = self.get_queryset().get(pk=value)
             except (ValueError, self.model.DoesNotExist):
+                try: # Check the rest of the values before returning None
+                    item = self.model.objects.get(pk=value)
+                except (ValueError, self.model.DoesNotExist):
+                    pass
                 item = None
         return item
 
