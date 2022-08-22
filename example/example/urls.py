362742e8ded0a792e53fd435d55afcb542277004
Mario Orlandi <morlandi@brainstorm.it>
2022-08-22 10:08:09 +0200

    Fix urls

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import include, url
+#from django.conf.urls import include, url
+from django.urls import include
+from django.urls import re_path as url
 from django.contrib import admin
 
 
--- selectable/tests/urls.py
+++ selectable/tests/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import handler404, handler500, include, url
+#from django.conf.urls import handler404, handler500, include, url
+from django.conf.urls import handler404, handler500
+from django.urls import include
+from django.urls import re_path as url
 
 
 handler404 = 'selectable.tests.views.test_404'
