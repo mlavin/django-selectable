653b727d1c1da36a3d49f5ba26c6be035f869b9b
Domenico DiNicola <dom.dinicola@gmail.com>
2023-06-22 11:36:28 +0200

    github actions

--- example/example/settings.py
+++ example/example/settings.py
@@ -9,12 +9,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": "example.db",  # Or path to database file if using sqlite3.
-        "USER": "",  # Not used with sqlite3.
-        "PASSWORD": "",  # Not used with sqlite3.
-        "HOST": "",  # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",  # Set to empty string for default. Not used with sqlite3.
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "example.db",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
